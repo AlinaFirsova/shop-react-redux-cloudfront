f4b6f67f99a294b2b4449df19cf5ca3d6e5caa7e
AlinaFirsova <firsova.a@winsolutions.ru>
2023-02-14 00:37:13 +0300

    Module 3

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -25,7 +25,7 @@ export default function Header() {
 
   return (
     <AppBar position="relative">
-      <Toolbar>
+      <Toolbar style={{ backgroundColor: "#F1B550" }}>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           <Link
             component={RouterLink}
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            Буше
           </Link>
         </Typography>
 
