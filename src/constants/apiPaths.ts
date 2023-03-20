c0090bc5bb45cf938660759c6c909a51c65a6423
AlinaFirsova <firsova.a@winsolutions.ru>
2023-03-21 00:30:22 +0300

    Module 7

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://jgvs8angoc.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://eltjo87zpg.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://7frprl4u94.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://9um7oh6vv6.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
